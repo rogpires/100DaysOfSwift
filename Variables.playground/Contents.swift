1924b8f5b2e8785dc8bbfe20ab253bf097931111
Rogerio Cesar Gomes Pires <rogeriocpires@hotmail.com>
2026-02-23 07:28:29 -0300

    Adds initial variables playground
    Creates a playground to demonstrate the usage of variables
    and printing their values in Swift.

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+var name = "Rogerio"
+var age = 21
+var burns = """
+    test and text
+    Helo
+    Oi
+    """
+var weight = 72.5
+var city = "Araaraquara"
+var message = "Eu moro em \(city)"
+
+print(name)
+print(age)
+print(burns)
+print(message)
